2dffc6c339bc3d52786217426404bd34a4aad52d
MonirSaikat <moniruzzamansaikat@yahoo.com>
2022-12-20 13:36:37 +0600

    env firebase config

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,12 +1,12 @@
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBzTBRfuZx-PyhVH9nkCOeRbfSjapc7128",
-  authDomain: "fizz-food-7405b.firebaseapp.com",
-  projectId: "fizz-food-7405b",
-  storageBucket: "fizz-food-7405b.appspot.com",
-  messagingSenderId: "882010464592",
-  appId: "1:882010464592:web:fb93685dc813660ef88c55"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROEJCT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 export const initFirebase = () => {
